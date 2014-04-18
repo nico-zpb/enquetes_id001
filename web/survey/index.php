c0dbe4fbd237be733a54f00b53af84a5a423883e
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-04-18 17:19:01 +0200

    debut stats

--- web/survey/index.php
+++ web/survey/index.php
@@ -17,15 +17,36 @@
     (_ (_   |   |   |  |   |   |
       (__<  |mm_|mm_|  |mm_|mm_|
 */
+session_start();
+
+include_once "../../php/functions.php";
+
+if(!isConnected()){
+    header("Location: /index.php");
+    die();
+}
+
+include_once "../../php/header.php";
+include_once "../../php/navbar.php";
 
 ?>
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Les statistiques - Les Jardins de Beauval - Enquêtes Clientèle</title>
-</head>
-<body>
 
-</body>
-</html>
+<div class="jumbotron">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="/img/logo3.png" alt="Les Jardins de Beauval"/>
+            </div>
+            <div class="col-md-10">
+                <h1>Statistiques</h1>
+            </div>
+            <div class="col-md-10 col-md-offset-2"></div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+
+    </div>
+</div>
