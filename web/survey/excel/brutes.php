4cea18e4fc1e878fde5fefa56f6b9488c104cea7
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-05-05 13:05:30 +0200

    sauvegarde

--- web/survey/excel/brutes.php
+++ web/survey/excel/brutes.php
@@ -104,6 +104,86 @@ $activeSheet->getStyle($columnNames[0]."1:".$columnNames[$numCols]."1")->applyFr
     ]
 ]);
 
+
+
+$startRow = 2;
+$endRow = $startRow;
+foreach($allClients as $key=>$client){
+
+    $sqlSejour = "SELECT * FROM sejours WHERE client_id=:id";
+    $sejourStmt = $pdo->prepare($sqlSejour);
+    $sejourStmt->bindValue(":id", $client["id"]);
+    $sejourStmt->execute();
+    $sejour = $sejourStmt->fetch();
+
+    $activeSheet->getRowDimension($endRow)->setRowHeight(30);
+    $endRow++;
+    /// comment connu hotel
+    $sql = "SELECT type_id FROM client_connaissance_type WHERE client_id=:id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(":id", $client["id"]);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    if($result){
+        $connaissanceTypesStr = "/ ";
+        foreach($result as $l=>$type){
+            $connaissanceTypesStr .= $connaissance_types[$type["type_id"] - 1] . " / ";
+
+            if($type["type_id"] == 11){
+                $subSql = "SELECT * FROM connaissance_types_custom WHERE client_id=:id";
+                $subStmt = $pdo->prepare($subSql);
+                $subStmt->bindValue(":id", $client["id"]);
+                $subStmt->execute();
+                $subResult = $subStmt->fetchAll();
+                if($subResult){
+                    //var_dump($subResult[0]);
+                    $activeSheet->setCellValueByColumnAndRow(1, $startRow+$key, $subResult[0]["name"]);
+                }
+            }
+        }
+
+        $activeSheet->setCellValueByColumnAndRow(0, $startRow+$key, $connaissanceTypesStr);
+
+    }
+    // departement numero
+    if($client["departement_num"]){
+        $activeSheet->setCellValueByColumnAndRow(2, $startRow+$key, $client["departement_num"]);
+    }
+
+    // pays etranger ?
+    if((int)$client["departement_num"] == 100){
+        $activeSheet->setCellValueByColumnAndRow(3, $startRow+$key, $client["pays"]);
+    }
+
+    // temps trajet
+    if($client["tps_trajet"]){
+        $activeSheet->setCellValueByColumnAndRow(4, $startRow+$key, $datas_tps_trajet[$client["tps_trajet"]-1]);
+    }
+
+    //type de chambre
+    if($sejour && $sejour["type_chambre"]>0){
+        $activeSheet->setCellValueByColumnAndRow(5, $startRow+$key, $datas_type_chambre[$sejour["type_chambre"]-1]);
+    }
+
+    //mois d'arrivee
+    if($client["arrive_mois"]){
+        $activeSheet->setCellValueByColumnAndRow(6, $startRow+$key, ucfirst($datas_mois[$client["arrive_mois"]-1]));
+    }
+
+
+
+
+}
+$activeSheet->getStyle($columnNames[0].$startRow.":".$columnNames[$numCols].$endRow)->applyFromArray([
+    "font"=>[
+        "name"=>"Calibri",
+        "size"=>11,
+    ],
+    "alignment"=>[
+        "wrap"=>true,
+        "vertical"=>PHPExcel_Style_Alignment::VERTICAL_TOP,
+    ],
+]);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// sauvegarde
